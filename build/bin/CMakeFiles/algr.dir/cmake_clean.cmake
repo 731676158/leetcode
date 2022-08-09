60c568e1805a4d590349145ef44cfd57f64ed59d
houkaiming_i <houkaiming_i@didiglobal.com>
2022-08-09 19:03:27 +0800

    first

---
+++ build/bin/CMakeFiles/algr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algr.dir/algorithm.o"
+  "algr"
+  "algr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
